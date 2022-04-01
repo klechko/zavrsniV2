77fc9fc92c89e5cee1bc9c42734b36129093bc11
klechko <tin.klecina@gmail.com>
2022-04-01 13:18:25 +0200

    spring commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const index = window;
+const division = document.querySelector('main > div');
+
+const sensitivity = 25;
+const media = window.matchMedia('(min-width: 1200px)');
+
+const parallax = (e, media) => {
+    if(media.matches){
+        const x = e.clientX;
+        const y = e.clientY;
+        division.style.transform = `translate(
+            ${x / sensitivity}px,
+            ${y / sensitivity}px
+            )`;
+    }
+    else {
+        division.style.transform = 'translate(0, 0)';
+    }
+}
+
+index.addEventListener('mousemove', e => {
+    parallax(e, media);
+    // media.addListener(parallax);
+});
